e85dd618b9d6c9047700cfb6ad267f49b263220c
Victor Granda <victorgranda1992@gmail.com>
2023-01-19 00:26:17 +0100

    Add correcttions suggested by EsLint and update rules for exceptions

--- projectsPrecourse/bingoGame/bingoGame.js
+++ projectsPrecourse/bingoGame/bingoGame.js
@@ -1,220 +1,256 @@
-let bombo = 0; let carta = []; let historialBombo = [];
-let rows = []; let counterRows = {first: 0, second: 0, third: 0}
+let bombo = 0;
+let carta = [];
+// eslint-disable-next-line no-unused-vars
+let rows = getRows();
+const historialBombo = [];
+const counterRows = { first: 0, second: 0, third: 0 };
 // // let isBingo = false, playBingo = false, nextTurn = false;
 // // let user = "", puntuation = 1000, matches = 0, turns = 0;
 const bingoDisplay = {
-    isBingo : false, 
-    playBingo : true,
-    nextTurn : false,
-}
-let keepPlaying = !bingoDisplay.isBingo && bingoDisplay.playBingo
+  isBingo: false,
+  playBingo: true,
+  nextTurn: false,
+};
+const keepPlaying = !bingoDisplay.isBingo && bingoDisplay.playBingo;
 
-const userDisplay = {  
+const userDisplay = {
+  user: "",
+  puntuation: 1000,
+  matches: 0,
+  turns: 0,
+};
 
-    user : "", 
-    puntuation : 1000, 
-    matches : 0, 
-    turns : 0, 
-   
-}
+const leaderBoard = () => {
+  const board = new Map([
+    ["Maria", 500],
+    ["Victor", 600],
+    ["Roberto", 700],
+    ["Anna", 800],
+  ]);
+  if (bingoDisplay.isBingo === true) {
+    board.set(userDisplay.user, userDisplay.puntuation);
+  }
 
-const leaderBoard = () =>{
+  const sortedLeaderBoard = new Map([...board].sort((a, b) => b[1] - a[1]));
+  console.log("------------------------LEADERBOARD------------------------");
+  return sortedLeaderBoard.forEach((value, key) => {
+    console.log(`${key} => ${value} puntos!`);
+  });
+};
 
-    const board = new Map([["Maria", 500], ["Victor", 600], ["Roberto", 700], ["Anna", 800]])
-    if (bingoDisplay.isBingo == true){board.set(userDisplay.user, userDisplay.puntuation)}
-    const sortedLeaderBoard = new Map([...board].sort((a, b) => b[1] - a[1]));
-    console.log("------------------------LEADERBOARD------------------------") 
-    return sortedLeaderBoard.forEach((value, key) => {console.log(`${key} => ${value} puntos!`)
-      })
-}
+const wellcome = () => {
+  userDisplay.user = prompt(`Hola! Por favor introduce tu nombre`);
+  while (
+    userDisplay.user === null ||
+    userDisplay.user === undefined ||
+    userDisplay.user.match(/\d+/) ||
+    userDisplay.user === ""
+  ) {
+    alert("Debes introducir un nombre válido");
+    userDisplay.user = prompt("Introduce tu nombre.");
+  }
 
-const wellcome = () =>{
+  alert(`Saludos ${userDisplay.user}!`);
+  if (confirm("Quieres jugar al bingo?")) {
+    console.log("Iniciando bingo...");
+    bingoDisplay.playBingo = true;
+  } else {
+    confirm("Quieres salir?");
+    fareWell();
+  }
+};
 
-    userDisplay.user = prompt(`Hola! Por favor introduce tu nombre`)
-    while (userDisplay.user === null || userDisplay.user === undefined || userDisplay.user.match(/\d+/) || userDisplay.user === "") {
-        alert("Debes introducir un nombre válido")
-        userDisplay.user = prompt("Introduce tu nombre.")
-    }
-    alert(`Saludos ${userDisplay.user}!`)
-    if (confirm("Quieres jugar al bingo?")){
-        console.log("Iniciando bingo...");
-        bingoDisplay.playBingo = true;
-        return 
+const bingoCard = () => {
+  let randomNum = 0;
+  for (let i = 1; carta.length < 15; i++) {
+    randomNum = getRandomNumber(1, 25);
+    if (carta.includes(randomNum)) {
+      i--;
+      randomNum = 0;
     } else {
-        confirm("Quieres salir?")
-        fareWell();
-        return 
+      carta.push(randomNum);
     }
+  }
+
+  alert(`Tu carta de juego es:\n ${getRows()} `); /* ${carta.join('  ')} */
+  if (confirm("Quieres otro cartón de juego distinto?")) {
+    carta = [];
+    return bingoCard(1, 16);
+  }
+};
+
+const getRows = () => {
+  const firstRow = new Array(carta.slice(0, 5).join(" "));
+  const secondRow = new Array(carta.slice(5, 10).join(" "));
+  const thirdRow = new Array(carta.slice(10, 15).join(" "));
+  rows = [firstRow, secondRow, thirdRow];
+  return [`${firstRow} \n ${secondRow} \n ${thirdRow}`];
+};
+
+const checkLinesRows = () => {
+  const row1 = carta.slice(0, 5);
+  const row2 = carta.slice(5, 10);
+  const row3 = carta.slice(10, 15);
+  if (!row1.some(Number) && counterRows.first === 0) {
+    counterRows.first++;
+    return console.log(
+      "------------------------LINEA FILA 1------------------------"
+    );
+  }
+
+  if (!row2.some(Number) && counterRows.second === 0) {
+    counterRows.second++;
+    return console.log(
+      "------------------------LINEA FILA 2------------------------"
+    );
+  }
+
+  if (!row3.some(Number) && counterRows.third === 0) {
+    counterRows.third++;
+    return console.log(
+      "------------------------LINEA FILA 3------------------------"
+    );
+  }
+};
+
+function getRandomNumber(min, max) {
+  let number;
+  while (bingoDisplay.playBingo && !bingoDisplay.isBingo) {
+    number = Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  return number;
 }
 
-const bingoCard = () =>{
-    let randomNum = 0
-    for (let i = 1; carta.length < 15; i++) {
-        randomNum = getRandomNumber(1, 25)        
-        if(carta.includes(randomNum)) { i--; randomNum = 0
-        } else {carta.push(randomNum)} 
-    }  
-    alert(`Tu carta de juego es:\n ${getRows()} `) /* ${carta.join('  ')} */
-    if(confirm("Quieres otro cartón de juego distinto?")){
-        carta = [];
-        return bingoCard(1,16)
-    }
-}
-
-const getRows = () => { 
-    let firstRow = new Array ( carta.slice(0, 5).join(" ")) ;
-    let secondRow = new Array (carta.slice(5, 10).join(" "));
-    let thirdRow = new Array (carta.slice(10, 15).join(" "));
-    rows = [firstRow, secondRow, thirdRow];
-    return [`${firstRow} \n ${secondRow} \n ${thirdRow}`];
-}
-
-
-const checkLinesRows = () => { 
-    let row1 = carta.slice(0,5)
-    let row2 = carta.slice(5,10)
-    let row3 = carta.slice(10,15)
-    if (!row1.some(Number) && counterRows.first === 0) {
-        counterRows.first++
-        return console.log("------------------------LINEA FILA 1------------------------")
-    }
-    if (!row2.some(Number) && counterRows.second === 0){
-        counterRows.second++
-        return console.log("------------------------LINEA FILA 2------------------------")
-    }
-    if (!row3.some(Number)&& counterRows.third === 0){
-        counterRows.third++
-        return console.log("------------------------LINEA FILA 3------------------------")
-    }
-}
-
-
-
-function getRandomNumber(min, max){
-    while (bingoDisplay.playBingo && !bingoDisplay.isBingo) {
-        const number = Math.floor(Math.random() * (max - min +1)) + min;
-        return number;
-    }  
-}
-
-const bomboRandom = (min, max) =>{
-    let pulledNumbers = 0
-    do{
-        pulledNumbers = getRandomNumber(1, 25);
-        while (!historialBombo.includes(pulledNumbers) && bingoDisplay.playBingo) {
-            historialBombo.push(pulledNumbers)
-            bombo = pulledNumbers;
-            return alert (`Número sacado del bombo es: \n el ${pulledNumbers}`) 
-        }
-        if (historialBombo.includes(pulledNumbers) && bingoDisplay.playBingo){
-           return bomboRandom();
-        }
-    }
-    while (carta.length == 18 && !bingoDisplay.isBingo && bingoDisplay.playBingo)
-}
-
-const matchNumBombo = () =>{
-    const index = carta.indexOf(bombo);   
-    carta.forEach(element => {
-        if (element == bombo){
-            carta[index] = "X"                
-            console.log(`* Genial! El ${bombo} ésta en tu cartón de juego: \n ${getRows()}`) /* porsiaca ${carta.join(' ')} */
-            userDisplay.matches ++;
-            }
-        if (index === -1 && bombo !== 0){
-             console.log(`Lástima! \n el número ${bombo} no está en tu carta`)
-            }
-        });    
-}
-
-const newTurn = () =>{
-    while(!bingoDisplay.isBingo && bingoDisplay.playBingo){
-        if (confirm("¿Quieres sacar otra bola?")){
-        userDisplay.turns ++;
-        userDisplay.puntuation -= 15;
-        bingoDisplay.nextTurn = true;
-        return console.clear();
-        }else {
-            bingoDisplay.nextTurn = false
-        return fareWell()
-        }  
-        
-    }
-    // let seguir = false;
-    // if (confirm("¿Quieres sacar otra bola?")){
-    //     seguir = true;
-    //     userDisplay.turns ++;
-    //     userDisplay.puntuation -= 15;
-    //     bingoDisplay.nextTurn = true;
-    //     return console.clear();
-    // } else {
-    //         bingoDisplay.nextTurn = false
-    //     return fareWell()
-    //     }  
-}
-const refreshConsole = () =>{
-console.log(`\n \n \n \n \n Ok ${userDisplay.user}, vas por el turno ${userDisplay.turns}, con ${userDisplay.matches} aciertos y ${userDisplay.puntuation} puntos.`);
-}
-
-const bingoWin = () =>{
-    if(userDisplay.matches === 15){
-        alert(`Bingo!`);
-        bingoDisplay.isBingo = true;
-        bingoDisplay.playBingo = false
-       return endBingoCard();
-    } else if(!bingoDisplay.isBingo) {
-        return newTurn();
-    }
-}
-
-const endBingoCard = () =>{
-     console.log(`¡¡ Genial ${userDisplay.user}, has completado tu cartón en ${userDisplay.turns} turnos, felicidades!!`)
-     leaderBoard();
-     return console.log(`Gracias por jugar ${userDisplay.user}!`)
-      
-}
-
-const tutorialPuntuation = () =>{
-    return  console.log(`La puntuación inicial es de ${userDisplay.puntuation} \n y por cada turno se irá restando 15puntos. \n el jugador con más puntuación, ganará.`)
-}
-
-const fareWell = () =>{
-    let goodBye = `Adiós ${userDisplay.user}, gracias por jugar!`
-
-    if (confirm("Confirma para salir")){
-        bingoDisplay.playBingo = false
-       return console.log(goodBye);
-    } else{
-        if (confirm("Reiniciar el juego?")){
-            return initGame()
-        }else {
-            return console.log(goodBye); 
-        }
+const bomboRandom = (min, max) => {
+  let pulledNumbers = 0;
+  do {
+    pulledNumbers = getRandomNumber(min, max);
+    while (!historialBombo.includes(pulledNumbers) && bingoDisplay.playBingo) {
+      historialBombo.push(pulledNumbers);
+      bombo = pulledNumbers;
+      alert(`Número sacado del bombo es: \n el ${pulledNumbers}`);
     }
 
-}
+    if (historialBombo.includes(pulledNumbers) && bingoDisplay.playBingo) {
+      return bomboRandom();
+    }
+  } while (
+    carta.length === 18 &&
+    !bingoDisplay.isBingo &&
+    bingoDisplay.playBingo
+  );
+};
 
-const initGame = () =>{
-   wellcome();
-   leaderBoard();
-   switch (bingoDisplay.playBingo) {
+const matchNumBombo = () => {
+  const index = carta.indexOf(bombo);
+  carta.forEach((element) => {
+    if (element === bombo) {
+      carta[index] = "X";
+      console.log(
+        `* Genial! El ${bombo} ésta en tu cartón de juego: \n ${getRows()}`
+      ); /* Porsiaca ${carta.join(' ')} */
+      userDisplay.matches++;
+    }
+
+    if (index === -1 && bombo !== 0) {
+      console.log(`Lástima! \n el número ${bombo} no está en tu carta`);
+    }
+  });
+};
+
+const newTurn = () => {
+  while (!bingoDisplay.isBingo && bingoDisplay.playBingo) {
+    if (confirm("¿Quieres sacar otra bola?")) {
+      userDisplay.turns++;
+      userDisplay.puntuation -= 15;
+      bingoDisplay.nextTurn = true;
+      console.clear();
+    }
+
+    bingoDisplay.nextTurn = false;
+    fareWell();
+  }
+};
+
+const bingoWin = () => {
+  if (userDisplay.matches === 15) {
+    alert(`Bingo!`);
+    bingoDisplay.isBingo = true;
+    bingoDisplay.playBingo = false;
+    return endBingoCard();
+  }
+
+  if (!bingoDisplay.isBingo) {
+    return newTurn();
+  }
+};
+
+const endBingoCard = () => {
+  console.log(
+    `¡¡ Genial ${userDisplay.user}, has completado tu cartón en ${userDisplay.turns} turnos, felicidades!!`
+  );
+  leaderBoard();
+  return console.log(`Gracias por jugar ${userDisplay.user}!`);
+};
+
+const tutorialPuntuation = () =>
+  console.log(
+    `La puntuación inicial es de ${userDisplay.puntuation} \n y por cada turno se irá restando 15puntos. \n el jugador con más puntuación, ganará.`
+  );
+
+const fareWell = () => {
+  const goodBye = `Adiós ${userDisplay.user}, gracias por jugar!`;
+
+  if (confirm("Confirma para salir")) {
+    bingoDisplay.playBingo = false;
+    return console.log(goodBye);
+  }
+
+  if (confirm("Reiniciar el juego?")) {
+    return initGame();
+  }
+
+  return console.log(goodBye);
+};
+
+const initGame = () => {
+  wellcome();
+  leaderBoard();
+  switch (bingoDisplay.playBingo) {
     case true:
-        tutorialPuntuation(), bingoCard(),bingo();
-        break;
+      tutorialPuntuation();
+      bingoCard();
+      bingo();
+      break;
     case false:
-        if(!bingoDisplay.playBingo){
-            fareWell();
-        }
-        break;
-   }
-}
+      if (!bingoDisplay.playBingo) {
+        fareWell();
+      }
 
-const bingo = () =>{
-   do{
-    console.log(keepPlaying); 
-    console.log(`${userDisplay.user} tu puntuación: ${userDisplay.puntuation} \n y aciertos: ${userDisplay.matches}`)
+      break;
+    default:
+      break;
+  }
+};
+
+const bingo = () => {
+  do {
+    console.log(keepPlaying);
+    console.log(
+      `${userDisplay.user} tu puntuación: ${userDisplay.puntuation} \n y aciertos: ${userDisplay.matches}`
+    );
     console.log(`${getRows()}`);
-    matchNumBombo(), checkLinesRows(), bingoWin(), bomboRandom();
-    } while (!bingoDisplay.isBingo && bingoDisplay.nextTurn && bingoDisplay.playBingo)   
-}
+
+    matchNumBombo();
+    checkLinesRows();
+    bingoWin();
+    bomboRandom();
+  } while (
+    !bingoDisplay.isBingo &&
+    bingoDisplay.nextTurn &&
+    bingoDisplay.playBingo
+  );
+};
+
 initGame();
--- projectsPrecourse/pasapalabraSimple/pasapalabra.js
+++ projectsPrecourse/pasapalabraSimple/pasapalabra.js
@@ -1,102 +1,116 @@
 import { questionPack } from "./questionPack";
 
-let questions = questionPack[Math.floor(Math.random() * 3)]
+let questions = questionPack[Math.floor(Math.random() * 3)];
 const gameInfo = {
-    userName : null,
-    userAnswer : '',
-    userPasapalabra : 0,
-    userWannaPlay : true,
-    wordPlaying : 0,
-    wordsAnswered : 0,
-    wordsCorrect: 0,
-    wordsTotal : questions.length,
-    maxTime : 130000,
-    ranking : [
-      {player : 'Mark', score : 12},
-      {player : 'Julia', score : 10},
-    ],
-    actualTime : null,
-}
+  userName: null,
+  userAnswer: "",
+  userPasapalabra: 0,
+  userWannaPlay: true,
+  wordPlaying: 0,
+  wordsAnswered: 0,
+  wordsCorrect: 0,
+  wordsTotal: questions.length,
+  maxTime: 130000,
+  ranking: [
+    { player: "Mark", score: 12 },
+    { player: "Julia", score: 10 },
+  ],
+  actualTime: null,
+};
 
-const resetGameInfo = () =>{
+const resetGameInfo = () => {
   gameInfo.userPasapalabra = 0;
   gameInfo.wordsAnswered = 0;
   gameInfo.wordsCorrect = 0;
   gameInfo.wordPlaying = 0;
-}
+};
 
-const userName = () => { 
-    const wellcome = `Hola! Bienvenido a Pasapalabra, porfavor introduce tu nombre`
-    if (gameInfo.userWannaPlay == true){
-          while(gameInfo.userName == '' || gameInfo.userName == null || gameInfo.userName == undefined){
-          gameInfo.userName = prompt(wellcome)
-        }
-    resetGameInfo();
-    alert(`usuario ${gameInfo.userName}`)
-    return showRules();
+const userName = () => {
+  const wellcome = `Hola! Bienvenido a Pasapalabra, porfavor introduce tu nombre`;
+  if (gameInfo.userWannaPlay === true) {
+    while (
+      gameInfo.userName === "" ||
+      gameInfo.userName === null ||
+      gameInfo.userName === undefined
+    ) {
+      gameInfo.userName = prompt(wellcome);
     }
-}
+
+    resetGameInfo();
+    alert(`usuario ${gameInfo.userName}`);
+    return showRules();
+  }
+};
 
 const showRules = () => {
   console.clear();
-  alert(`Hola ${gameInfo.userName}, a continuación se te mostrará una definición de una palabra que deberás adivinar.
-  \n-Cada acierto sumará un punto.\n-El tiempo maximo de la partida són ${gameInfo.maxTime/1000} segundos.\n
+  alert(`Hola ${
+    gameInfo.userName
+  }, a continuación se te mostrará una definición de una palabra que deberás adivinar.
+  \n-Cada acierto sumará un punto.\n-El tiempo maximo de la partida són ${
+    gameInfo.maxTime / 1000
+  } segundos.\n
   -Cuando aparezcan las definiciones: 
           ·Escribe pasapalabra si no conoces la respuesta y quieres continuar con la siguiente.
           ·Escribe "end" para finalizar la partida actual.`);
-}
+};
 
-const confirmPlay= () => {
-  gameInfo.userWannaPlay = confirm(`Quieres empezar a jugar ${gameInfo.userName}?`)
-  return gameInfo.userWannaPlay
-}
+const confirmPlay = () => {
+  gameInfo.userWannaPlay = confirm(
+    `Quieres empezar a jugar ${gameInfo.userName}?`
+  );
+  return gameInfo.userWannaPlay;
+};
 
 const startGame = () => {
-  gameTimer()
-  while(gameInfo.userWannaPlay == true && gameInfo.wordsCorrect !=questions.length ){
-      initRound()
+  gameTimer();
+  while (
+    gameInfo.userWannaPlay === true &&
+    gameInfo.wordsCorrect !== questions.length
+  ) {
+    initRound();
   }
-}
+};
 
 const gameTimer = () => {
-  gameInfo.actualTime = Date.now()
-}
+  gameInfo.actualTime = Date.now();
+};
 
 const initRound = () => {
-  if (gameInfo.wordsAnswered == gameInfo.wordsTotal){
-    return gameInfo.userWannaPlay = false;
+  if (gameInfo.wordsAnswered === gameInfo.wordsTotal) {
+    gameInfo.userWannaPlay = false;
+    return;
   }
-  for ( letter in questions ){
+
+  for (letter in questions) {
     switch (true) {
       case gameInfo.actualTime + gameInfo.maxTime <= Date.now():
-        alert('TIME`S UP!!');
+        alert("TIME`S UP!!");
         gameInfo.userWannaPlay = false;
         return;
       case gameInfo.wordsCorrect == gameInfo.wordsTotal:
         return;
       case questions[letter].status == 0:
         userAnswer();
-        if (gameInfo.userAnswer === null || gameInfo.userAnswer == 'end'){
+        if (gameInfo.userAnswer === null || gameInfo.userAnswer == "end") {
           gameInfo.userWannaPlay = false;
-          return;
         }
-        answerManager();
-    }
-  }
-}
-
+        default : false;
+      }
+      answerManager();
+};
 
 const userAnswer = () => {
-    gameInfo.userAnswer = prompt(`${questions[letter].question}`);
-    if (gameInfo.userAnswer != null){
-      gameInfo.userAnswer = cleanString(gameInfo.userAnswer);
-  };
+  gameInfo.userAnswer = prompt(`${questions[letter].question}`);
+  if (gameInfo.userAnswer !== null) {
+    gameInfo.userAnswer = cleanString(gameInfo.userAnswer);
+  }
 };
 
 const answerManager = () => {
   switch (gameInfo.userAnswer) {
     case `pasapalabra`:
-      gameInfo.userPasapalabra +=1;
+      gameInfo.userPasapalabra += 1;
       alert(`Siguiente pregunta! \n
        ${gameInfo.wordsCorrect}/${gameInfo.wordsTotal} aciertos
        \n ${gameInfo.userPasapalabra} pasapalabras`);
@@ -104,67 +118,80 @@ const answerManager = () => {
     case questions[letter].answer:
       questions[letter].status = "succes";
       gameInfo.wordsCorrect += 1;
-      gameInfo.wordsAnswered += 1; 
-      alert(`Correcto! ${gameInfo.wordsCorrect}/${gameInfo.wordsTotal} aciertos`); 
+      gameInfo.wordsAnswered += 1;
+      alert(
+        `Correcto! ${gameInfo.wordsCorrect}/${gameInfo.wordsTotal} aciertos`
+      );
       break;
     default:
       questions[letter].status = `fail`;
-      gameInfo.wordsAnswered += 1; 
-      alert(`Incorrecto, la respuesta correcta es: ${questions[letter].answer}`);
+      gameInfo.wordsAnswered += 1;
+      alert(
+        `Incorrecto, la respuesta correcta es: ${questions[letter].answer}`
+      );
   }
 };
-
 
 const sortQuestions = () => {
   questions = questionPack[Math.floor(Math.random() * 3)];
-  return gameInfo.wordsTotal = questions.length
-}
-
-const rankingPlayer = () => {
-   let count = 0;
-   for (letter in questions){
-    if (questions[letter].status == `succes`){
-      count +=1;
-    };
-    questions[letter].status = 0;
-   }
-   gameInfo.ranking.push({player : gameInfo.userName, score: count});
+  gameInfo.wordsTotal = questions.length;
 };
 
-const showRanking= (gameInfo) => {
-  rankingPlayer()
-  console.log("Ranking:")
-  for (let i = 0; i < gameInfo.ranking.length; i++) {
-    console.log(`${i + 1} position. ${gameInfo.ranking[i].player}: ${gameInfo.ranking[i].score} points`)
-  }
-}
+const rankingPlayer = () => {
+  let count = 0;
+  for (letter in questions) {
+    if (questions[letter].status == `succes`) {
+      count += 1;
+    }
 
+    questions[letter].status = 0;
+  }
+
+  gameInfo.ranking.push({ player: gameInfo.userName, score: count });
+};
+
+const showRanking = (gameInfo) => {
+  rankingPlayer();
+  console.log("Ranking:");
+  for (let i = 0; i < gameInfo.ranking.length; i++) {
+    console.log(
+      `${i + 1} position. ${gameInfo.ranking[i].player}: ${
+        gameInfo.ranking[i].score
+      } points`
+    );
+  }
+};
 
 const cleanString = (string) => {
-  string = string.toLowerCase().normalize("NFD").replace(/\p{Diacritic}/gu,"");
+  string = string
+    .toLowerCase()
+    .normalize("NFD")
+    .replace(/\p{Diacritic}/gu, "");
   return string;
-}
+};
 
 const fareWell = () => {
   console.log(`Gracias por utilizar ésta app`);
   return resetGameInfo();
-}
+};
 
 const playAgain = () => {
- confirm(`Quieres volver a jugar?`) ?
-  gameInfo.userWannaPlay = true : fareWell();
-}
+  confirm(`Quieres volver a jugar?`)
+    ? (gameInfo.userWannaPlay = true)
+    : fareWell();
+};
 
 const initFunction = () => {
-  do{
-    userName()
-    if(confirmPlay()== true){
-        sortQuestions()
-        startGame();
-        showRanking(gameInfo);
-      };
+  do {
+    userName();
+    if (confirmPlay() == true) {
+      sortQuestions();
+      startGame();
+      showRanking(gameInfo);
+    }
+
     playAgain();
-    }while(gameInfo.userWannaPlay == true);
+  } while (gameInfo.userWannaPlay == true);
 };
 
 initFunction()
